0e020098e85f3ac5cc4707508226267e221f61c6
senthiltaker <senthilkumarstar3@gmail.com>
2016-08-12 09:47:08 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class positive
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a==0)
+{
+System.out.println("given number is zero");
+}
+else if(a<0)
+{
+System.out.println("the given number is negative");
+}
+else
+{
+System.out.prinln("the given number is positive");
+}
+}}
